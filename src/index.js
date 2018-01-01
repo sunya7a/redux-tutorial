e5177463f4b95b5b412014be502e7794ed03dc66
Jens Jap <jens_jap@brown.edu>
2018-01-01 20:02:13 +0800

    Redux: Reducer Composition with combineReducers().

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore } from 'redux';
-//import createStore from './lib/createStore';
+import { createStore, combineReducers } from 'redux';
 import counter from './components/counter/counter';
 import todos from './components/todo/todos';
 import visibilityFilter from './components/visibilityFilter/visibilityFilter';
 import { Provider } from 'react-redux';
 //import CounterShow from './components/counter/CounterShow';
 
-const root = (state = {}, action) => {
-  return {
-    counter: counter(
-      state.counter,
-      action
-    ),
-    todos: todos(
-      state.todos,
-      action
-    ),
-    visibilityFilter: visibilityFilter(
-      state.visibilityFilter,
-      action
-    )
-  };
-};
+const root = combineReducers({
+  counter,
+  todos,
+  visibilityFilter
+});
+
+//const root = (state = {}, action) => {
+//  return {
+//    counter: counter(
+//      state.counter,
+//      action
+//    ),
+//    todos: todos(
+//      state.todos,
+//      action
+//    ),
+//    visibilityFilter: visibilityFilter(
+//      state.visibilityFilter,
+//      action
+//    )
+//  };
+//};
 
 const store = createStore(root);
 const render = () => {
