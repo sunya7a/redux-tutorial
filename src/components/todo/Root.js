f1c7e166ee6305c42e2d9f2152bd547f353d0ad7
Jens Jap <jens_jap@brown.edu>
2018-01-03 14:06:12 +0800

    Redux: Adding React Router to the Project.

--- src/components/todo/Root.js
+++ src/components/todo/Root.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import TodoApp from './TodoApp';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <TodoApp />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={TodoApp} />
+      </Switch>
+    </BrowserRouter>
   </Provider>
 );
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+};
 
 export default Root;
