1cb99d6e20a4cc6f8fa979c1bc894fce313b5f0b
Jens Jap <jens_jap@brown.edu>
2018-01-05 00:07:36 +0800

    Redux: Applying Redux Middleware.

--- src/lib/configureStore.js
+++ src/lib/configureStore.js
@@ -1,51 +1,26 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import { createLogger } from 'redux-logger';
 
 import counter from '../components/counter/counter';
 import todos, * as fromTodos from '../components/todo/todos';
-
-const logger = (store) => (next) => {
-  if (!console.group) {
-    return next;
-  }
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = next(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
-
-const promise = (store) => (next) => (action) => {
-  if (typeof action.then === 'function') {
-    return action.then(next);
-  }
-  return next(action);
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware =>
-    store.dispatch = middleware(store)(store.dispatch)
-  );
-};
 
 const configureStore = () => {
   const root = combineReducers({
     counter,
     todos
   });
-  const store = createStore(root)
+
   const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares);
-
-  return store
+  return createStore(
+    root,
+    applyMiddleware(...middlewares)
+  );
 };
 
 export default configureStore;
