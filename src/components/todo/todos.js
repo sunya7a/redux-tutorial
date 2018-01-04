81204dc146999b3cdfbb289dae4ef72d56a05601
Jens Jap <jens_jap@brown.edu>
2018-01-04 14:44:19 +0800

    Redux: Normalizing the State Shape.

--- src/components/todo/todos.js
+++ src/components/todo/todos.js
@@ -1,57 +1,59 @@
-const todo = (state, action) => {
+import { combineReducers } from 'redux';
+import todo from './todoReducer';
+
+const byId = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      };
     case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
-
       return {
         ...state,
-        completed: !state.completed
+        [action.id]: todo(state[action.id], action)
       };
     default:
       return state;
   }
 };
 
-const todos = (state = [], action) => {
+const allIds = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        todo(undefined, action)
+        action.id
       ];
-    case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
     default:
       return state;
   }
 };
 
+const todos = combineReducers({
+  byId,
+  allIds
+});
+
 export default todos;
+
+const getAllTodos = (state) =>
+  state.allIds.map(id => state.byId[id]);
 
 export const getVisibleTodos = (
   state,
   filter
 ) => {
+  const allTodos = getAllTodos(state)
+
   switch (filter) {
     case 'all':
-      return state;
+      return allTodos;
     case 'active':
-      return state.filter(
+      return allTodos.filter(
         t => !t.completed
       );
     case 'completed':
-      return state.filter(
+      return allTodos.filter(
         t => t.completed
       );
     default:
-      return state;
+      return allTodos;
   }
 };
--- src/components/todo/todos.test.js
+++ src/components/todo/todos.test.js
@@ -1,14 +1,17 @@
 import todos from './todos';
 
 it('adds a todo', () => {
-  const stateBefore = [];
-  const stateAfter = [
-    {
-      id: 1,
-      text: 'Learn redux',
-      completed: false
+  const stateBefore = {};
+  const stateAfter = {
+    allIds: [1],
+    byId: {
+      1: {
+        id: 1,
+        text: 'Learn redux',
+        completed: false
+      }
     }
-  ];
+  };
   const action = {
     type: 'ADD_TODO',
     id: 1,
@@ -24,30 +27,36 @@ it('adds a todo', () => {
 });
 
 it('toggles completed for todo id', () => {
-  const stateBefore = [
-    {
-      id: 1,
-      text: 'Learn redux',
-      completed: false
-    },
-    {
-      id: 2,
-      text: 'Buy milk',
-      completed: false
-    },
-  ];
-  const stateAfter = [
-    {
-      id: 1,
-      text: 'Learn redux',
-      completed: false
-    },
-    {
-      id: 2,
-      text: 'Buy milk',
-      completed: true
-    },
-  ];
+  const stateBefore = {
+    allIds: [1, 2],
+    byId: {
+      1: {
+        id: 1,
+        text: 'Learn redux',
+        completed: false
+      },
+      2: {
+        id: 2,
+        text: 'Let\'s go',
+        completed: false
+      }
+    }
+  };
+  const stateAfter = {
+    allIds: [1, 2],
+    byId: {
+      1: {
+        id: 1,
+        text: 'Learn redux',
+        completed: false
+      },
+      2: {
+        id: 2,
+        text: 'Let\'s go',
+        completed: true
+      }
+    }
+  };
   const action = {
     type: 'TOGGLE_TODO',
     id: 2
