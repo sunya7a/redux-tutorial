fcf0b92ca3a4c0cb2bd65402864a3c0b28858079
Jens Jap <jens_jap@brown.edu>
2018-01-06 01:39:06 +0800

    Redux: Dispatching Actions Asynchronously with Thunks.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,6 @@
 import { v4 } from 'node-uuid';
 import * as api from '../api';
 
-export const requestTodos = (filter) => ({
-  type: 'REQUEST_TODOS',
-  filter,
-});
-
-const receiveTodos = (filter, response) => ({
-  type: 'RECEIVE_TODOS',
-  filter,
-  response,
-});
-
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
   id: v4(),
@@ -23,7 +12,19 @@ export const toggleTodo = (id) => ({
   id,
 });
 
-export const fetchTodos = (filter) =>
-  api.fetchTodos(filter).then((response) =>
-    receiveTodos(filter, response)
-  );
+const requestTodos = (filter) => ({
+  type: 'REQUEST_TODOS',
+  filter,
+});
+const receiveTodos = (filter, response) => ({
+  type: 'RECEIVE_TODOS',
+  filter,
+  response,
+});
+export const fetchTodos = (filter) => (dispatch) => {
+  dispatch(requestTodos(filter));
+
+  return api.fetchTodos(filter).then((response) => {
+    dispatch(receiveTodos(filter, response));
+  });
+};
